58dc5358154999fe8c4ffe134cc605075df0b39d
Julian Klotz <julian.klotz@outdooractive.com>
2017-05-10 09:07:29 +0200

    Hut, collapsable

--- baseline/src/form.js
+++ baseline/src/form.js
@@ -1,3 +1,6 @@
+var body = document.getElementsByTagName('body')[0];
+body.classList.add( 'js-enabled' );
+
 var inputs = document.querySelectorAll( '.file-control' );
 Array.prototype.forEach.call( inputs, function( input ) {
 	var label	 = input.nextElementSibling,
@@ -9,3 +12,15 @@ Array.prototype.forEach.call( inputs, function( input ) {
 		label.querySelector( '.js-filename' ).innerHTML = fileName;
 	});
 });
+
+
+$( '.collapse-toggle' ).on( 'click', function(evt) {
+	var toggle = $( evt.target );
+	toggle.closest( '.collapse-block' ).toggleClass( 'js-open' );
+});
+
+
+
+
+
+
